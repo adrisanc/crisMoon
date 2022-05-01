d969015e351f8b65c4cbb8b0300c11ee1b54fc38
YASMIN ADRIANA ORJUELA SANCHEZ <yasminadrianasanchez@gmail.com>
2022-05-01 14:09:54 -0500

    Animations & Jump
    Fix jump and add idle, run & jump animations.

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -4,19 +4,47 @@ public class PlayerMovement : MonoBehaviour
 {
     [SerializeField] private float speed;
     private Rigidbody2D body;
+    private Animator anim;
+    private bool grounded;
 
     private void Awake()
-    {
+    {   
+        //get references from game object
         body = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
     }
 
     private void Update()
     {
-        body.velocity = new Vector2(Input.GetAxis("Horizontal") * speed, body.velocity.y);
+        float horizontalInput = Input.GetAxis("Horizontal");
+        body.velocity = new Vector2(horizontalInput * speed, body.velocity.y);
 
-        if (Input.GetKey(KeyCode.Space))
-            body.velocity = new Vector2(body.velocity.x, speed);
+        if (horizontalInput > 0.01f)
+            transform.localScale = Vector3.one;
+        //flip player when moving left-right
+        else if (horizontalInput <- 0.01f)
+            transform.localScale = new Vector3(-1,1,1);
 
+        if (Input.GetKey(KeyCode.Space) && grounded)
+            Jump();
+
+        //set animatir parameters
+        anim.SetBool("run", horizontalInput != 0);
+        anim.SetBool("grounded", grounded);
+
+    }
+
+    private void Jump()
+    {
+        body.velocity = new Vector2(body.velocity.x, speed);
+        anim.SetTrigger("jump");
+        grounded = false;
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Ground")
+            grounded = true;
     }
 
 }
