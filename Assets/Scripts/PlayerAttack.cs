ea2a7102c2c59d8fd7166e03bcd9b6a3244e46e2
YASMIN ADRIANA ORJUELA SANCHEZ <yasminadrianasanchez@gmail.com>
2022-05-04 01:04:24 -0500

    Graphics Level Design
    graphics level design implemented

---
+++ Assets/Scripts/PlayerAttack.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+
+public class PlayerAttack : MonoBehaviour
+{
+    [SerializeField] private float attackCooldown;
+    [SerializeField] private Transform ballonPoint;
+    [SerializeField] private GameObject[] ballons;
+
+    private Animator anim;
+    private PlayerMovement playerMovement;
+    private float cooldownTimer = Mathf.Infinity;
+
+    private void Awake()
+    {
+        anim = GetComponent<Animator>();
+        playerMovement = GetComponent<PlayerMovement>();
+    }
+
+    private void Update()
+    {
+        if (Input.GetMouseButton(0) && cooldownTimer > attackCooldown)//if mouse botton is press
+            Attack();
+
+        cooldownTimer += Time.deltaTime;
+    }
+
+    private void Attack()
+    {
+        anim.SetTrigger("attack");
+        cooldownTimer = 0;
+
+        ballons[FindBallon()].transform.position = ballonPoint.position;
+        ballons[FindBallon()].GetComponent<BallonPlayer>().SetDirection(Mathf.Sign(transform.localScale.x));
+    }
+
+    private int FindBallon()
+    {
+        for (int i = 0; i < ballons.Length; i++)
+        {
+            if (!ballons[i].activeInHierarchy)
+                return i;
+        }
+        return 0;
+    }
+}
