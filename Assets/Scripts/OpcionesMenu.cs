50a0ac87b6f1b82360752e1a2abe73e8bb4ee308
YASMIN ADRIANA ORJUELA SANCHEZ <yasminadrianasanchez@gmail.com>
2022-05-15 13:08:13 -0500

    MenuUI
    Start Game option Implemented

---
+++ Assets/Scripts/OpcionesMenu.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class OpcionesMenu : MonoBehaviour
+{
+    public void Menu()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+}
